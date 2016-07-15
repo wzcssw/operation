87603b3b5d9b449a5e2a8f9eb4fc60c375352d90
王志成 <wangzhicheng@tongxinyiliao.com>
2016-07-15 15:01:04 +0800

    准备文件

--- app/api/index.js
+++ app/api/index.js
@@ -1,15 +1,8 @@
 function JoinedApi(app){
     "use strict";
     app.use(require('./user.js'));
-    app.use(require('./dic_hospitals.js'));
-    app.use(require('./projects.js'));
-    app.use(require('./cities.js'));
-    app.use(require('./bodies.js'));
-    app.use(require('./categories.js'));
-    app.use(require('./operation_logs.js'));
-    app.use(require('./body_modes.js'));
-    app.use(require('./hospitals.js'));
-    app.use(require('./manage_orders.js'));
+    app.use(require('./visit_records.js'));
+    app.use(require('./feedbacks.js'));
 }
 
 module.exports = JoinedApi;
